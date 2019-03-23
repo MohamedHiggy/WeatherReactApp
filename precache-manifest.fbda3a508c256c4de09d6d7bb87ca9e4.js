a083f9fdc1e099688cb6e4c3dc9db933d5cb6d65
MohamedHiggy <mohamed.higgy@.com>
2019-03-23 19:59:26 +0200

    Updates

---
+++ precache-manifest.fbda3a508c256c4de09d6d7bb87ca9e4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "504a4a1c4f265df9311a",
+    "url": "/WeatherReactApp/static/js/runtime~main.31f3dac8.js"
+  },
+  {
+    "revision": "64cc3b59819edf3830d6",
+    "url": "/WeatherReactApp/static/js/main.3772d304.chunk.js"
+  },
+  {
+    "revision": "b6bb956612567acc00c5",
+    "url": "/WeatherReactApp/static/js/2.e55ccc72.chunk.js"
+  },
+  {
+    "revision": "64cc3b59819edf3830d6",
+    "url": "/WeatherReactApp/static/css/main.1eca818e.chunk.css"
+  },
+  {
+    "revision": "848ca413041be7c8ad51a9f3cc6ab54c",
+    "url": "/WeatherReactApp/index.html"
+  }
+];
